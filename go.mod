bb51d8611485241bcb3b470922d3cfe478321c7d
Andres Morales <meliand11@yahoo.com>
2025-02-28 13:36:26 -0500

    profit calculator initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/profit_calc.go
+
+go 1.21.5
---
+++ profit_calc.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var revenue float64
+	var expenses float64
+	var taxRate float64
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Number of expenses: ")
+	fmt.Scan(&expenses)
+
+	fmt.Print("Taces to account for: ")
+	fmt.Scan(&taxRate)
+
+	EBT := revenue - expenses
+
+	taxes := EBT * taxRate
+
+	PAT := EBT - taxes
+
+	ratio := EBT / PAT
+
+	fmt.Print("EBT before taxes: ")
+	fmt.Println(EBT)
+
+	fmt.Print("Profit after taxes: ")
+	fmt.Println(PAT)
+
+	fmt.Print("Ratio ebt/profit: ")
+	fmt.Println(ratio)
+}
