b2a14d2a1d0f9d4c6f938707c6d1ff2dc9742d3c
Andres Morales <meliand11@yahoo.com>
2025-03-05 17:42:50 -0500

    adding functions

--- profit_calc.go
+++ profit_calc.go
@@ -9,29 +9,49 @@ func main() {
 	var expenses float64
 	var taxRate float64
 
-	fmt.Print("Investment Amount: ")
+	//fmt.Print("Investment Amount: ")
+	outputText("Investment Amount: ")
 	fmt.Scan(&revenue)
 
-	fmt.Print("Number of expenses: ")
+	//fmt.Print("Number of expenses: ")
+	outputText("Number of expenses: ")
 	fmt.Scan(&expenses)
 
-	fmt.Print("Taces to account for: ")
+	//fmt.Print("Taxes to account for: ")
+	outputText("Taxes to account for: ")
 	fmt.Scan(&taxRate)
 
+	//EBT := revenue - expenses
+
+	//taxes := EBT * (1 - taxRate/100)
+
+	//PAT := EBT - taxes
+
+	//ratio := EBT / PAT
+
+	EBTCalc, PATCalc, ratioCalc := calculating(revenue, expenses, taxRate)
+
+	//fmt.Print("EBT before taxes: ")
+	outputText("EBT before taxes: ")
+	fmt.Println(EBTCalc)
+
+	//fmt.Print("Profit after taxes: ")
+	outputText("Profit after taxes: ")
+	fmt.Println(PATCalc)
+
+	//fmt.Print("Ratio ebt/profit: ")
+	outputText("Ratio ebt/profit: ")
+	fmt.Println(ratioCalc)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
+}
+
+func calculating(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	EBT := revenue - expenses
-
 	taxes := EBT * (1 - taxRate/100)
-
 	PAT := EBT - taxes
-
 	ratio := EBT / PAT
-
-	fmt.Print("EBT before taxes: ")
-	fmt.Println(EBT)
-
-	fmt.Print("Profit after taxes: ")
-	fmt.Println(PAT)
-
-	fmt.Print("Ratio ebt/profit: ")
-	fmt.Println(ratio)
+	return EBT, PAT, ratio
 }
